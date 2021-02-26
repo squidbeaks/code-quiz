77485fba781d478e109c7dbf772ae4f2b58900c4
Leah Russell <leahsigridrussell@gmail.com>
2021-02-26 08:48:23 -0800

    adding question and answer objects; adding question and answer divs; separating add question and answer functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,109 @@
-var startButtonEl = document.querySelector("#start-btn");
-var codeInstructionsEl = document.querySelector("#code-instructions");
+// const from DOM
+const startButtonEl = document.querySelector("#start-btn");
+const questionBody = document.querySelector("#question-body");
+const codeInstructionsEl = document.querySelector("#code-instructions");
+const questionEl = document.querySelector("#question");
+const answer1El = document.querySelector("#answer1");
+const answer2El = document.querySelector("#answer2");
+const answer3El = document.querySelector("#answer3");
+const answer4El = document.querySelector("#answer4");
 
-var deleteEl = function(El) {
+var currentQuestionIndex = 0;
+
+const deleteEl = function(El) {
     El.remove();
 };
 
-var createTaskEl = function(taskDataObj) {
-    // create list item
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answers: {
+            a: "Strings",
+            b: "Booleans",
+            c: "Alerts",
+            d: "Numbers"
+            },
+        correctAnswer: "c",
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with ____________.",
+        answers: {
+            a: "quotes",
+            b: "curly brackets",
+            c: "paranthesis",
+            d: "square brackets"
+            },
+        correctAnswer: "c",
+    },
+    {
+        question: "Arrays in Javascript can be used to store __________.",
+        answers: {
+            a: "numbers and strings",
+            b: "other arrays",
+            c: "booleans",
+            d: "all of the above"
+            },
+        correctAnswer: "d",
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        answers: {
+            a: "commas",
+            b: "curly brackets",
+            c: "quotes",
+            d: "paranthesis"
+            },
+        correctAnswer: "c",
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: {
+            a: "JavaScript",
+            b: "terminal/bash",
+            c: "for loops",
+            d: "console.log"
+            },
+        correctAnswer: "d"
+    }
+];
 
-    // add task id as a custom attribute
-    listItemEl.setAttribute("data-task-id", taskIdCounter);
-
-    // create div to hold task info and add to list item
-    var taskInfoEl = document.createElement("div");
-    taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-    listItemEl.appendChild(taskInfoEl);
-
-    var taskActionsEl = createTaskActions(taskIdCounter);
-    listItemEl.appendChild(taskActionsEl);
-
-    tasksToDoEl.appendChild(listItemEl);
-
-    // add entire list item to list
-    tasksToDoEl.appendChild(listItemEl);
-
-    // increase task counter for next unique id
-    taskIdCounter++;
-};
-
-var addQandA = function() {
+var addQuestion = function() {
     // add h3 for question
-    var questionBody = document.querySelector("#question-body");
-    var questionEl = document.createElement("div");
-    questionEl.className = "question";
-    questionEl.innerHTML = "<h3 class='question'>Commonly used data types DO NOT include:</h3>"
-    questionBody.appendChild(questionEl);
-    // add 4 buttons with different answers to questions
-    var answerEl = document.createElement("div");
-    answerEl.className = "answer";
-    answerEl.innerHTML = "<button class='answer'>Strings</button><button class='answer'>Booleans</button><button class='answer'>Alerts</button><button class='answer'>Numbers</button>"
-    questionBody.appendChild(answerEl);
+    var question = document.createElement("h3");
+    question.className = "question";
+    question.innerHTML = questions[0].question;
+    questionEl.appendChild(question);
 };
 
-var changeTime = function() {
+var addAnswers = function() {
+    // answer 1 button
+    var answerEl = document.createElement("button");
+    answerEl.className = "answer";
+    answerEl.innerHTML = questions[0].answers.a
+    answer1El.appendChild(answerEl);
+
+    // answer 2 button
+    var answerEl = document.createElement("button");
+    answerEl.className = "answer";
+    answerEl.innerHTML = questions[0].answers.b
+    answer2El.appendChild(answerEl);
+
+    // answer 3 button
+    var answerEl = document.createElement("button");
+    answerEl.className = "answer";
+    answerEl.innerHTML = questions[0].answers.c
+    answer3El.appendChild(answerEl);
+
+    // answer 4 button
+    var answerEl = document.createElement("button");
+    answerEl.className = "answer";
+    answerEl.innerHTML = questions[0].answers.d
+    answer4El.appendChild(answerEl);
+};
+
+
+
+const changeTime = function() {
     var timerEl = document.querySelector("#timer");
     var timeLeft = 75;
 
@@ -57,15 +115,17 @@ var changeTime = function() {
         } else {
             timerEl.textContent = '';
             clearInterval(timeInterval);
+            alert('You have run out of time!');
         }
     }, 1000);
 };
 
-var startButtonHandler = function() {
+const startButtonHandler = function() {
     // remove coding quiz text & start button
     deleteEl(codeInstructionsEl);
     // add html elements for question and answer buttons
-    addQandA();
+    addQuestion();
+    addAnswers();
     // change the time (and make it count down)
     changeTime();
 };
@@ -73,4 +133,19 @@ var startButtonHandler = function() {
 // click start button
 startButtonEl.addEventListener("click", startButtonHandler);
 
+// once click answer button
+var clickAnswerButton = function() { 
+    var answerOb = answers.answer
+    answerOb.onclick = function() {
+    // skip to next question
+    if(answerButtonEl.textContent != "Alerts") {
+        // display "Wrong!" notification
+        // reduce time by 15 seconds
+       console.log("Boo!");
+    } else {
+        console.log("Yay!");
+        // display "Correct!" noitification
+    }
+}
+};
 
