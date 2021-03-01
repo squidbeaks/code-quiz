1696d72f08625cf63e5db45e965f3d9879f9c3ab
Leah Russell <leahsigridrussell@gmail.com>
2021-02-28 23:01:00 -0800

    Refactoring and adding styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var questions = [
+const questions = [
     {
         question: "Commonly used data types DO NOT include:",
         answers: {
@@ -52,48 +52,45 @@ var questions = [
 ];
 
 const startButtonEl = document.querySelector("#start-btn");
-const questionBody = document.querySelector("#question-body");
+const quizBody = document.querySelector("#quiz-body");
 const codeInstructionsEl = document.querySelector("#code-instructions");
+const questionEl = document.createElement("div");
+const answerEl = document.createElement("div");
+const countDown = document.querySelector("#countdown");
+const highScoresEl = document.querySelector("#high-scores");
+const timerEl = document.querySelector("#timer");
+
 var timeLeft = 75;
 var currentQuestionIndex = 0;
-var questionEl = document.createElement("div");
-var answerEl = document.createElement("div");
-var timerEl = document.querySelector("#timer");
-var highScoresEl = document.querySelector("#high-scores");
-var timeEl = document.querySelector("#time");
 var timeInterval;
 var name;
 var score;
+var notificationEl = document.createElement("div");
+notificationEl.id = "notification";
 
 const deleteEl = function(El) {
     El.remove();
 };
 
-var addQuestion = function(question) {
+const addQuestion = function(question) {
     questionEl.id = "question";
-    // add h3 for question
     questionEl.className = "question";
     questionEl.innerHTML = `<h3 class='question'>${question}</h3>`;
-    questionBody.appendChild(questionEl);
+
+    quizBody.appendChild(questionEl);
 };
 
-var addAnswers = function(answers) {
+const addAnswers = function(answers) {
     answerEl.id= "answer";
-    // answer buttons
-    answerEl.innerHTML = `<button class='answer' onclick="notification('a')">${answers.a}</button><button class='answer' onclick="notification('b')">${answers.b}</button><button class='answer' onclick="notification('c')">${answers.c}</button><button class='answer' onclick="notification('d')">${answers.d}</button>`
-    questionBody.appendChild(answerEl);
+    answerEl.innerHTML = `<button onclick="notification('a')">${answers.a}</button>
+    <button onclick="notification('b')">${answers.b}</button>
+    <button onclick="notification('c')">${answers.c}</button>
+    <button onclick="notification('d')">${answers.d}</button>`
+
+    quizBody.appendChild(answerEl);
 };
 
-function notification(answer) {
-    var p = document.createElement("div");
-    if (answer === questions[currentQuestionIndex].correctAnswer) {
-        p.innerHTML = "<p>Correct!</p>"
-    } else {
-        p.innerHTML = "<p>Wrong!</p>"
-        timeLeft -= 10;
-    }
-    questionBody.appendChild(p);
-
+const nextQuestion = function() {
     deleteEl(questionEl);
     deleteEl(answerEl);
     currentQuestionIndex++;
@@ -101,12 +98,29 @@ function notification(answer) {
         addQuestion(questions[currentQuestionIndex].question);
         addAnswers(questions[currentQuestionIndex].answers);
     }
+
+    if (notificationEl.querySelector("notification") != null) {
+        deleteEl(notificationEl);
+    }
+}
+
+const notification = function(answer) {
+    if (answer === questions[currentQuestionIndex].correctAnswer) {
+        notificationEl.innerHTML = "<p>Correct!</p>"
+    } else {
+        notificationEl.innerHTML = "<p>Wrong!</p>"
+        timeLeft -= 10;
+    }
+
+    quizBody.appendChild(notificationEl);
+
+    nextQuestion();
 };
 
 const changeTime = function() {
     timeInterval = setInterval(function() {
         if (timeLeft > 0) {
-            timerEl.textContent = timeLeft;
+            countDown.textContent = timeLeft;
             if (currentQuestionIndex >= questions.length) {
                 endQuiz();
             }
@@ -114,91 +128,79 @@ const changeTime = function() {
         }
         else {
             timeLeft = 0;
-            timerEl.textContent = timeLeft;
+            countDown.textContent = timeLeft;
             endQuiz();
         }
     }, 1000);
 };
 
 const startButtonHandler = function() {
-    // remove coding quiz text & start button
     deleteEl(codeInstructionsEl);
-    // add html elements for question and answer buttons
     addQuestion(questions[currentQuestionIndex].question);
     addAnswers(questions[currentQuestionIndex].answers);
-    // change the time (and make it count down)
     changeTime();
 };
 
-// click start button
-startButtonEl.addEventListener("click", startButtonHandler);
-
 const endQuiz = function() {
-    // when questions are out
-    // stop clock
     clearInterval(timeInterval);
 
     if (timeLeft < 0) {
         timeLeft = 0;
     }
 
-    console.log(timeLeft);
+    quizBody.innerHTML = "";
 
-    questionBody.innerHTML = "";
-    // display "<h3>All Done!</h3>"
-    const allDone = document.createElement("h3");
+    let allDone = document.createElement("h3");
+    allDone.id = "all-done";
     allDone.innerText = "All Done!"
-    questionBody.appendChild(allDone);
-    // display "<p> Your final score is ${timeLeft}"
-    const finalScore = document.createElement("p");
+
+    let finalScore = document.createElement("p");
     finalScore.innerText = `Your final score is ${timeLeft}`;
-    questionBody.appendChild(finalScore);
-    // display "Enter initials form and input with submit button"
-    const initialsText = document.createElement("p");
+
+    let initialsText = document.createElement("p");
     initialsText.innerText = "Enter initials";
-    questionBody.appendChild(initialsText);
 
-    const initialsEl = document.createElement("input");
+    let initialsEl = document.createElement("input");
     initialsEl.id = "initials";
-    questionBody.appendChild(initialsEl);
 
-    var submit = document.createElement("button");
+    let submit = document.createElement("button");
+    submit.className = "button";
     submit.innerText = "Submit";
     submit.addEventListener('click', saveScore);
-    questionBody.appendChild(submit);
+
+    quizBody.appendChild(allDone);
+    quizBody.appendChild(finalScore);
+    quizBody.appendChild(initialsText);
+    quizBody.appendChild(initialsEl);
+    quizBody.appendChild(submit);
 };
 
 const saveScore = function() {
-    //leaderBoard = [name, score];
-    name = document.getElementById("initials").value;
+    initials = document.getElementById("initials").value;
     score = timeLeft;
 
-    localStorage.setItem("name", name);
+    localStorage.setItem("initials", initials);
     localStorage.setItem("score", score);
 
-    //localStorage.setItem('leaderBoard', JSON.stringify(leaderBoard));
-    //var leaderBoard = JSON.parse(localStorage.getItem('leaderBoard'));
-
-    questionBody.innerHTML = "";
+    quizBody.innerHTML = "";
     displayScores();
 };
 
 const displayScores = function() {
     deleteEl(highScoresEl);
-    deleteEl(timeEl);
+    deleteEl(timerEl);
     if (localStorage.getItem("score") === null) {
-        questionBody.innerHTML =
+        quizBody.innerHTML =
         `<h2>There is no high score yet! Play the game to claim your title!<h2>`;
     } else {
-        questionBody.innerHTML =
-        `<h2>` + localStorage.getItem("name") + " has the high score of " + localStorage.getItem("score") + `</h2>
+        quizBody.innerHTML =
+        `<h2>` + localStorage.getItem("initials") + " has the high score of " + localStorage.getItem("score") + `</h2>
         <button onclick="clearScore()">Clear score!</button><button onclick="resetGame()">Play Again!</button>`;
     }
 }
 
 const clearScore = function() {
     localStorage.clear();
-
     resetGame();
 }
 
@@ -211,16 +213,6 @@ const highScoreLink = function () {
     displayScores();
 };
 
-highScoresEl.addEventListener("click", highScoreLink);
+startButtonEl.addEventListener("click", startButtonHandler);
 
-//GIVEN I am taking a code quiz
-//WHEN I click the start button
-//THEN a timer starts and I am presented with a question
-//WHEN I answer a question
-//THEN I am presented with another question
-//WHEN I answer a question incorrectly
-//THEN time is subtracted from the clock
-//WHEN all questions are answered or the timer reaches 0
-//THEN the game is over
-//WHEN the game is over
-//THEN I can save my initials and score
+highScoresEl.addEventListener("click", highScoreLink);
